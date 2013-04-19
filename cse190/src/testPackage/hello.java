eb46cbfae8daa4cb7d3bcdf09a1787e867c8184e
Yos Sugianto <ysugiant@ucsd.edu>
2013-04-18 22:25:09 -0700

    Revert "Cleaning"
    This reverts commit 7b573d9508874d481a2ef9b47b828aa0d413608f.

---
+++ cse190/src/testPackage/hello.java
@@ -0,0 +1,19 @@
+package testPackage;
+
+import java.io.*; 
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+@WebServlet("/hello")
+public class hello extends HttpServlet {
+@Override
+	public void doGet(HttpServletRequest request,
+		HttpServletResponse response)
+				throws ServletException, IOException {
+				PrintWriter out = response.getWriter();
+				out.println("Hello World");
+	}
+		
+	
+}
+
