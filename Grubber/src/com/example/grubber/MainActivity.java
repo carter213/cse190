cf2c1dc57d6dc89684619db5148ec682633014d7
Gina Chen <ginachen615@gmail.com>
2013-04-29 11:38:55 -0700

    Search stuff

--- Grubber/src/com/example/grubber/MainActivity.java
+++ Grubber/src/com/example/grubber/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onSearchRequested();
     }
 
 
@@ -40,4 +41,7 @@ public class MainActivity extends Activity {
     	startActivity(intent);     	
     }
     
+    public void openSearchDialog(View view) {
+    	onSearchRequested();
+    }
 }
