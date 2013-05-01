73bdd358af4a41a1d4a607dc219882b850c2065d
Gina Chen <ginachen615@gmail.com>
2013-05-01 10:24:59 -0700

    blargh

--- Grubber/gen/com/example/grubber/R.java
+++ Grubber/gen/com/example/grubber/R.java
@@ -24,15 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f09000b;
-        public static final int action_settings=0x7f09000c;
+        public static final int action_forgot_password=0x7f09000a;
+        public static final int action_settings=0x7f09000b;
         public static final int email=0x7f090003;
         public static final int login=0x7f090005;
         public static final int login_form=0x7f090002;
         public static final int login_status=0x7f090000;
         public static final int login_status_message=0x7f090001;
         public static final int main_search=0x7f090008;
-        public static final int map=0x7f09000a;
         public static final int nearby=0x7f090009;
         public static final int password=0x7f090004;
         public static final int register_button=0x7f090007;
